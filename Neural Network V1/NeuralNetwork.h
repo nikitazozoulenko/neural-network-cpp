ce3aaeca47aa48051ed73b846c31bd41d36099ef
nikitazozoulenko <nikita.zozoulenko@gmail.com>
2017-05-22 17:24:38 +0200

    start

---
+++ Neural Network V1/NeuralNetwork.h
@@ -0,0 +1,20 @@
+#ifndef NEURAL_NETWORK_V1_H
+#define NEURAL_NETWORK_V1_H
+
+#define NUM_HIDDEN_LAYERS 3
+#define NUM_LAYERS NUM_HIDDEN_LAYERS+2
+
+#define SIZE_INPUT_LAYER 30
+#define SIZE_HIDDEN_LAYER_1 40
+#define SIZE_HIDDEN_LAYER_2 50
+#define SIZE_HIDDEN_LAYER_3 30
+#define SIZE_OUTPUT_LAYER 2
+
+class NeuralNetwork
+{
+public:
+	NeuralNetwork();
+protected:
+private:
+};
+#endif //NEURAL_NETWORK_V1_H
--- Neural Network V1/main.cpp
+++ Neural Network V1/main.cpp
@@ -1,14 +1,25 @@
 #include <iostream>
-#include <Eigen/Dense>
+//#include <Eigen/Dense>
 
-using Eigen::MatrixXd;
+#include "NeuralNetwork.h"
+
+
+
 int main()
 {
-	MatrixXd m(2, 2);
-	m(0, 0) = 3;
-	m(1, 0) = 2.5;
-	m(0, 1) = -1;
-	m(1, 1) = m(1, 0) + m(0, 1);
-	std::cout << m << std::endl;
-	std::cin.get();
+	NeuralNetwork network();
+
+	return 0;
 }
+
+//using Eigen::MatrixXd;
+//int main()
+//{
+//	MatrixXd m(2, 2);
+//	m(0, 0) = 3;
+//	m(1, 0) = 2.5;
+//	m(0, 1) = -1;
+//	m(1, 1) = m(1, 0) + m(0, 1);
+//	std::cout << m << std::endl;
+//	std::cin.get();
+//}
